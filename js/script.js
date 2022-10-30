1ca098641f418993196a2e17f556f1d549e45b8e
Code Ninja <fbook3603@gmail.com>
2022-10-30 20:15:44 +0600

    erron check

--- js/script.js
+++ js/script.js
@@ -1,43 +1,59 @@
 const allPlayers = () => {
   document.getElementById("spinner").style.display = "block";
-  const searchValue = document.getElementById("search-box").value;
-  const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => showPlayerDetails(data.player));
+  const searchInput = document.getElementById("search-box");
+  const searchValue = searchInput.value;
+  searchInput.value = "";
+  if (searchValue === "") {
+    document.getElementById("spinner").style.display = "none";
+    document.getElementById("errorText").style.display = "block";
+    document.getElementById("player-container").innerHTML = "";
+    document.getElementById("detailsPlayers").innerHTML = "";
+  } else {
+    document.getElementById("errorText").style.display = "none";
+    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => showPlayerDetails(data.player));
+  }
 };
 const showPlayerDetails = (players) => {
-  if (players) {
+  if (players === null) {
+    document.getElementById("errorText").style.display = "block";
     document.getElementById("spinner").style.display = "none";
-    document.getElementById("footer").style.display = "block";
-  }
-  const parent = document.getElementById("player-container");
-  parent.textContent = "";
-  players.forEach((player) => {
-    if (player.strThumb !== null) {
-      const div = document.createElement("div");
-      div.classList.add("col");
-      div.classList.add("my-3");
-      div.innerHTML = `
-        <div class="card" style="width: 18rem;">
-        <img w-50 src="${player.strThumb}" class="card-img-top " alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Name: ${player.strPlayer}cool</h5>
-          <p class="card-text">Country: ${player.strNationality}</p>
-          <div class="allbutton ">
-            <button class="btn btn-danger">
-                Delete
-            </button>
-            <button onClick="details('${player.idPlayer}')" class="btn btn-success">
-                Details
-            </button>
+    document.getElementById("player-container").innerHTML = "";
+    document.getElementById("detailsPlayers").innerHTML = "";
+    // console.log(players);
+  } else {
+    document.getElementById("spinner").style.display = "none";
+    // console.log(players);
+    const parent = document.getElementById("player-container");
+    parent.textContent = "";
+    players.forEach((player) => {
+      if (player.strThumb !== null) {
+        const div = document.createElement("div");
+        div.classList.add("col");
+        div.classList.add("my-3");
+        div.innerHTML = `
+            <div class="card" style="width: 18rem;">
+            <img w-50 src="${player.strThumb}" class="card-img-top " alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Name: ${player.strPlayer}cool</h5>
+              <p class="card-text">Country: ${player.strNationality}</p>
+              <div class="allbutton ">
+                <button class="btn btn-danger">
+                    Delete
+                </button>
+                <button onClick="details('${player.idPlayer}')" class="btn btn-success">
+                    Details
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-        `;
-      parent.appendChild(div);
-    }
-  });
+            `;
+        parent.appendChild(div);
+      }
+    });
+  }
 };
 const details = (info) => {
   const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${info}`;
@@ -49,8 +65,7 @@ const details = (info) => {
 const setDetails = (id) => {
   //   console.log(id);
   const detailsContainer = document.getElementById("details-container");
-  const maleImage = document.getElementById("maleImage");
-  detailsContainer.textContent = "";
+  detailsContainer.innerHTML = "";
   if (id.strGender == "Male") {
     document.getElementById("maleImage").style.display = "block";
     document.getElementById("femaleImage").style.display = "none";
